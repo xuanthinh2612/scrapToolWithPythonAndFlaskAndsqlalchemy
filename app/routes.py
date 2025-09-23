cb97c776202bdde419419d6f410908e5d2125b4f
Thinh Tran <xuanthinh2623@gmail.com>
2025-09-23 21:11:09 +0900

    add file

--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,7 @@ bp = Blueprint('main', __name__)
 
 @bp.route("/")
 def index():
-    session = current_app.db_session
-    products = session.query(Product).all()
-    return render_template("index.html", products=products)
+    # session = current_app.db_session
+    # products = session.query(Product).all()
+    # return render_template("index.html", products=products)
+    return render_template("index.html")
